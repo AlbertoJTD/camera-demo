8b3e3f5dc7905d78b6d6b75e1d3c34b4615fac9c
AlbertoJTD <albertojtd@outlook.com>
2023-07-16 09:51:19 -0600

    Add button to take pictures

--- CameraDemo/CameraDemo.Android/MainActivity.cs
+++ CameraDemo/CameraDemo.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace CameraDemo.Droid
 {
@@ -13,17 +14,16 @@ namespace CameraDemo.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 
-			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
 			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
--- CameraDemo/CameraDemo/MainPage.xaml.cs
+++ CameraDemo/CameraDemo/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,26 @@ namespace CameraDemo
 		public MainPage()
 		{
 			InitializeComponent();
+			TakePhoto.Clicked += TakePhoto_Clicked;
 		}
-	}
+
+		private async void TakePhoto_Clicked(object sender, EventArgs e)
+		{
+			var cameraOptions = new StoreCameraMediaOptions();
+			cameraOptions.PhotoSize = PhotoSize.Small;
+			cameraOptions.SaveToAlbum = true;
+
+			//var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+			var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(cameraOptions);
+
+
+			if (photo != null )
+			{
+				PhotoTag.Source = ImageSource.FromStream(() =>
+				{
+					return photo.GetStream();
+				});
+			}
+        }
+    }
 }
